1fa656c6c511c3c1814768b8d797e2b18a3553b7
Toufu <157666279+QuangZu@users.noreply.github.com>
2025-07-24 16:18:04 +0700

    version1.0

--- client/src/api/Subject.js
+++ client/src/api/Subject.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendURL = process.env.VUE_APP_API_URL || 'http://localhost:3000/api';
+const backendURL = 'https://stardy-3old.onrender.com/api';
 
 const getAuthHeader = () => {
     const token = localStorage.getItem('token');
